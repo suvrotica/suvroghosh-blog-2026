fea588ffdfc09892ca8ea736c81cc25d69d11b90
suvrotica <117360356+suvrotica@users.noreply.github.com>
2025-12-01 18:39:18 +0530

    analytics speed insight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+ 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+  	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+  	import { dev } from '$app/environment';
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
@@ -8,5 +13,6 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
 
 {@render children()}
