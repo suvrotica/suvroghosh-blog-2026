3377722e7b29a7a27d26db4c6d37fbd7bf43149b
Suvro Ghosh <117360356+suvrotica@users.noreply.github.com>
2025-12-06 07:26:59 +0530

    Update +page.svelte

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -6,9 +6,14 @@
 
 <svelte:head>
 	<title>Gallery - SuvroGhosh.Blog</title>
+	<meta name="description" content="A visual collection." />
 </svelte:head>
 
-<div class="container mx-auto py-8">
-	<h1 class="text-4xl font-bold text-center mb-8 font-serif">Gallery</h1>
+<section class="container mx-auto py-8">
+	<h1 class="text-center mb-2 text-4xl font-serif font-bold">Gallery</h1>
+    <p class="text-center text-neutral-500 dark:text-neutral-400 mb-8 italic">
+        A visual collection from the archives.
+    </p>
+    
 	<ImageMasonry images={data.images} />
-</div>
+</section>
