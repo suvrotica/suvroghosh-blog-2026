2b0ab2465c240757eb8d8c73bc77ce7ca67db06d
Suvro Ghosh <117360356+suvrotica@users.noreply.github.com>
2026-01-02 03:49:43 +0530

    Update Footer.svelte

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -4,6 +4,6 @@
 
 <footer class="border-t border-neutral-300 dark:border-neutral-700 bg-neutral-100 dark:bg-neutral-900 p-6 text-center">
 	<p class="text-sm text-neutral-600 dark:text-neutral-400">
-		&copy; {year} Suvro Ghosh — A Goodly Holy Hindoo Who Diarizes in Vomit, Piss & Shit.
-	</p>
+		&copy; {year} Suvro Ghosh (सुव्रो घोषः) — पावनः शोभनः च हिन्दूः, यः जर्नं विष्ठां मूत्रं च लेखयति।
+    </p>
 </footer>
